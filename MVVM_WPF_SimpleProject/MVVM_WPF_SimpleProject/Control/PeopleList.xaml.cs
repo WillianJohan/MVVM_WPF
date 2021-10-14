106cd4def917577ea32d2f8825d2f955044dbd1a
Willian Johan <johan_willian@hotmail.com>
2021-10-14 11:42:18 -0300

    imp - userControl PeopleList created

---
+++ MVVM_WPF_SimpleProject/MVVM_WPF_SimpleProject/Control/PeopleList.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace MVVM_WPF_SimpleProject.Control
+{
+    public partial class PeopleList : UserControl
+    {
+        public PeopleList()
+        {
+            InitializeComponent();
+        }
+    }
+}
