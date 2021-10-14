cfd91a49206fe1ff9080a4a1648a6161f3d64137
Willian Johan <johan_willian@hotmail.com>
2021-10-14 11:44:02 -0300

    imp - UserControl of PersonRegistration

---
+++ MVVM_WPF_SimpleProject/MVVM_WPF_SimpleProject/Control/PersonRegistration.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+
+namespace MVVM_WPF_SimpleProject.Control
+{
+
+    public partial class PersonRegistration : UserControl
+    {
+        public PersonRegistration()
+        {
+            InitializeComponent();
+        }
+    }
+}
