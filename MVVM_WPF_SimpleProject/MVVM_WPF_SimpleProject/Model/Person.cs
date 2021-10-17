9bc829bf92f2e6b2ffeed72391e65050fcff0cd7
Willian Johan <johan_willian@hotmail.com>
2021-10-17 15:06:51 -0300

    imp - Implementação inicial de person

--- MVVM_WPF_SimpleProject/MVVM_WPF_SimpleProject/Model/Person.cs
+++ MVVM_WPF_SimpleProject/MVVM_WPF_SimpleProject/Model/Person.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace MVVM_WPF_SimpleProject.Model
 {
     public class Person
     {
+        [PrimaryKey, AutoIncrement, NotNull] public int ID { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Phone { get; set; }
+        public string CPF { get; set; }
 
     }
 }
